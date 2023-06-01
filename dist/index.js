83d66317137fbd6ae03f1cea5826480b686ec283
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 14:26:40 +0600

    setter and getters in ts

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,24 @@
 //   }
 // }
 class User {
-    constructor(email, name) {
+    constructor(email, name // private userId: string
+    ) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Rangpur";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const ahsan = new User("ah@san.com", "ahsan");
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,25 @@
 //   }
 // }
 class User {
+  private _courseCount = 1;
+
   readonly city: string = "Rangpur";
   constructor(
     public email: string,
-    public name: string
-  ) // private userId: string
-  {}
+    public name: string // private userId: string
+  ) {}
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+  get courseCount(): number {
+    return this._courseCount;
+  }
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const ahsan = new User("ah@san.com", "ahsan");
