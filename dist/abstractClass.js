1badf3264b4969cc96b9ce0c3a459909a1afa04e
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 14:58:03 +0600

    abstruct class in ts

---
+++ dist/abstractClass.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        return 8;
+    }
+}
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+const ahsan = new Instagram("test", "test", 3);
+console.log(ahsan.getReelTime());
---
+++ src/abstractClass.ts
@@ -0,0 +1,25 @@
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+  abstract getSepia(): void;
+  getReelTime(): number {
+    return 8;
+  }
+}
+
+class Instagram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter);
+  }
+  getSepia(): void {
+    console.log("Sepia");
+  }
+}
+
+const ahsan = new Instagram("test", "test", 3);
+
+console.log(ahsan.getReelTime());
+export {};
