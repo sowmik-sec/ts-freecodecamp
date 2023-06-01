036dee0ec40473ecb0d973c9e5531160a23a1e0e
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 16:21:33 +0600

    Discriminated Union and Exhaustiveness Checking with never

--- dist/detection.js
+++ dist/detection.js
@@ -40,3 +40,25 @@ function getFood(pet) {
         return "bird food";
     }
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    else if (shape.kind === "rectangle") {
+        return shape.length * shape.width;
+    }
+    return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
