4a85b0115bdc0c63c536a8e764fec1ddbfdd62c8
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 14:33:56 +0600

    protected in ts

--- dist/index.js
+++ dist/index.js
@@ -32,4 +32,13 @@ class User {
         this._courseCount = courseNum;
     }
 }
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
 const ahsan = new User("ah@san.com", "ahsan");
