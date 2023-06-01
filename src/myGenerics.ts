590f16c09e27b3161df18c812f9b09c44c8725dc
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 15:16:13 +0600

    generics in ts

---
+++ src/myGenerics.ts
@@ -0,0 +1,28 @@
+const score: Array<number> = [];
+
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+
+function identityTwo(val: any): any {
+  return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+  return val;
+}
+
+identityThree("3");
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+interface Bootle {
+  brand: string;
+  type: number;
+}
+
+// identityFour<Bootle>({})
